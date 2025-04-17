992d0f8e00c992b26b0c27d03aee5c90046d0261
zjncoo <151642330+zjncoo@users.noreply.github.com>
2025-04-18 00:53:08 +0200

    updates

--- AM_DECOR/amdecor/branding-project.js
+++ AM_DECOR/amdecor/branding-project.js
@@ -144,3 +144,22 @@ document.addEventListener('DOMContentLoaded', () => {
       window.location.href = randomHref;
     }
   });
+
+  //smooth scrolling
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
+  smooth: true,
+  direction: 'vertical',
+  gestureDirection: 'vertical',
+  smoothTouch: true,
+  touchMultiplier: 2,
+  infinite: false,
+});
+
+// Update on each frame
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
