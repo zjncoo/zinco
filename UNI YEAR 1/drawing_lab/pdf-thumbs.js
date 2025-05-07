148943d90c8ddd042422b20290deb30e432a657a
zjncoo <151642330+zjncoo@users.noreply.github.com>
2025-05-07 23:18:39 +0200

    ok

---
+++ UNI YEAR 1/drawing_lab/pdf-thumbs.js
@@ -0,0 +1,26 @@
+const pdfPaths = [
+    'img/cover vinile.pdf',
+    'img/uv.pdf',
+    'img/uv.pdf' // Add your paths here
+  ];
+  
+  const container = document.getElementById('pdfThumbContainer');
+  
+  pdfPaths.forEach(path => {
+    const canvas = document.createElement('canvas');
+    canvas.classList.add('pdf-thumb');
+    container.appendChild(canvas);
+  
+    const ctx = canvas.getContext('2d');
+    const scale = 1.2;
+  
+    pdfjsLib.getDocument(path).promise.then(pdfDoc => {
+      pdfDoc.getPage(1).then(page => {
+        const viewport = page.getViewport({ scale });
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+  
+        page.render({ canvasContext: ctx, viewport });
+      });
+    });
+  });
